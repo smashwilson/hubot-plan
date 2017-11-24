fca3d627f0518f6dcd8637ff8332cebd7c01da98
Ash Wilson <smashwilson@github.com>
2017-11-23 19:13:49 -0500

    Rework Event to use a single object and throw errors

--- test/event.test.js
+++ test/event.test.js
@@ -11,173 +11,173 @@ describe('Event', function () {
   const nextMonth = moment.tz('2017-12-16', moment.ISO_8601, 'America/New_York')
 
   describe('in the proposed state', function () {
-    let proposed
+    let evt
 
     beforeEach(function () {
-      proposed = new Event("Party at Frey's House")
-      proposed.proposeDate(tomorrow)
+      evt = new Event("Party at Frey's House")
+      evt.proposeDate(tomorrow)
     })
 
     it('adds proposed dates', function () {
-      proposed.proposeDate(nextWeek)
+      evt.proposeDate(nextWeek)
 
-      assert.deepEqual(proposed.proposalKeys(), [0, 1])
-      assert.equal(proposed.proposal(0).date(), tomorrow)
-      assert.equal(proposed.proposal(1).date(), nextWeek)
+      assert.deepEqual(evt.proposalKeys(), [0, 1])
+      assert.equal(evt.proposal(0).date(), tomorrow)
+      assert.equal(evt.proposal(1).date(), nextWeek)
     })
 
     it('removes proposed dates', function () {
-      proposed.proposeDate(nextMonth)
+      evt.proposeDate(nextMonth)
 
-      proposed.unpropose(0)
+      evt.unpropose(0)
 
-      assert.deepEqual(proposed.proposalKeys(), [1])
-      assert.equal(proposed.proposal(1).date(), nextMonth)
-      assert.isFalse(proposed.proposal(0).isValid())
+      assert.deepEqual(evt.proposalKeys(), [1])
+      assert.equal(evt.proposal(1).date(), nextMonth)
+      assert.throws(() => evt.proposal(0), 'Invalid proposed date')
     })
 
     it('adds invitees', function () {
-      proposed.invite('frey')
-      proposed.invite('fenris')
+      evt.invite('frey')
+      evt.invite('fenris')
 
-      assert.deepEqual(proposed.getInvitees(), ['frey', 'fenris'])
+      assert.deepEqual(evt.getInvitees(), ['frey', 'fenris'])
     })
 
     it('removes invitees', function () {
-      proposed.invite('frey')
-      proposed.invite('hubot')
-      proposed.invite('fenris')
+      evt.invite('frey')
+      evt.invite('hubot')
+      evt.invite('fenris')
 
-      proposed.uninvite('hubot')
+      evt.uninvite('hubot')
 
-      assert.deepEqual(proposed.getInvitees(), ['frey', 'fenris'])
+      assert.deepEqual(evt.getInvitees(), ['frey', 'fenris'])
     })
 
     it('tracks which invitees accept or reject proposed dates', function () {
-      proposed.invite('frey')
-      proposed.invite('fenris')
-      proposed.invite('reostra')
+      evt.invite('frey')
+      evt.invite('fenris')
+      evt.invite('reostra')
 
-      proposed.proposeDate(nextWeek)
-      proposed.proposeDate(nextMonth)
+      evt.proposeDate(nextWeek)
+      evt.proposeDate(nextMonth)
 
-      assert.equal(proposed.proposal(0).date(), tomorrow)
-      assert.equal(proposed.proposal(0).yesCount(), 0)
-      assert.isFalse(proposed.proposal(0).isLeading())
-      assert.equal(proposed.proposal(1).date(), nextWeek)
-      assert.equal(proposed.proposal(1).yesCount(), 0)
-      assert.isFalse(proposed.proposal(1).isLeading())
-      assert.equal(proposed.proposal(2).date(), nextMonth)
-      assert.equal(proposed.proposal(2).yesCount(), 0)
-      assert.isFalse(proposed.proposal(2).isLeading())
+      assert.equal(evt.proposal(0).date(), tomorrow)
+      assert.equal(evt.proposal(0).yesCount(), 0)
+      assert.isFalse(evt.proposal(0).isLeading())
+      assert.equal(evt.proposal(1).date(), nextWeek)
+      assert.equal(evt.proposal(1).yesCount(), 0)
+      assert.isFalse(evt.proposal(1).isLeading())
+      assert.equal(evt.proposal(2).date(), nextMonth)
+      assert.equal(evt.proposal(2).yesCount(), 0)
+      assert.isFalse(evt.proposal(2).isLeading())
 
-      proposed.acceptProposal('frey', 0)
-      proposed.acceptProposal('fenris', 0)
-      proposed.acceptProposal('fenris', 1)
-      proposed.acceptProposal('reostra', 0)
-      proposed.acceptProposal('reostra', 1)
-      proposed.acceptProposal('reostra', 2)
+      evt.acceptProposal('frey', 0)
+      evt.acceptProposal('fenris', 0)
+      evt.acceptProposal('fenris', 1)
+      evt.acceptProposal('reostra', 0)
+      evt.acceptProposal('reostra', 1)
+      evt.acceptProposal('reostra', 2)
 
-      assert.equal(proposed.proposal(0).date(), tomorrow)
-      assert.equal(proposed.proposal(0).yesCount(), 3)
-      assert.isTrue(proposed.proposal(0).isLeading())
-      assert.equal(proposed.proposal(1).date(), nextWeek)
-      assert.equal(proposed.proposal(1).yesCount(), 2)
-      assert.isFalse(proposed.proposal(1).isLeading())
-      assert.equal(proposed.proposal(2).date(), nextMonth)
-      assert.equal(proposed.proposal(2).yesCount(), 1)
-      assert.isFalse(proposed.proposal(2).isLeading())
+      assert.equal(evt.proposal(0).date(), tomorrow)
+      assert.equal(evt.proposal(0).yesCount(), 3)
+      assert.isTrue(evt.proposal(0).isLeading())
+      assert.equal(evt.proposal(1).date(), nextWeek)
+      assert.equal(evt.proposal(1).yesCount(), 2)
+      assert.isFalse(evt.proposal(1).isLeading())
+      assert.equal(evt.proposal(2).date(), nextMonth)
+      assert.equal(evt.proposal(2).yesCount(), 1)
+      assert.isFalse(evt.proposal(2).isLeading())
     })
 
     describe('becoming finalized', function () {
       beforeEach(function () {
-        proposed.invite('frey')
-        proposed.invite('fenris')
-        proposed.invite('reostra')
+        evt.invite('frey')
+        evt.invite('fenris')
+        evt.invite('reostra')
 
-        proposed.proposeDate(nextWeek)
-        proposed.proposeDate(nextMonth)
+        evt.proposeDate(nextWeek)
+        evt.proposeDate(nextMonth)
 
-        proposed.acceptProposal('frey', 0)
-        proposed.acceptProposal('fenris', 0)
-        proposed.acceptProposal('fenris', 1)
-        proposed.acceptProposal('reostra', 0)
-        proposed.acceptProposal('reostra', 1)
-        proposed.acceptProposal('reostra', 2)
+        evt.acceptProposal('frey', 0)
+        evt.acceptProposal('fenris', 0)
+        evt.acceptProposal('fenris', 1)
+        evt.acceptProposal('reostra', 0)
+        evt.acceptProposal('reostra', 1)
+        evt.acceptProposal('reostra', 2)
       })
 
       it('transitions invitees to yes or no based on their response to the final date', function () {
-        const finalized = proposed.finalize(1)
+        evt.finalize(1)
 
-        assert.isFalse(finalized.isAttending('frey'))
-        assert.isTrue(finalized.isAttending('fenris'))
-        assert.isTrue(finalized.isAttending('reostra'))
+        assert.isFalse(evt.finalProposal().isAttending('frey'))
+        assert.isTrue(evt.finalProposal().isAttending('fenris'))
+        assert.isTrue(evt.finalProposal().isAttending('reostra'))
       })
     })
   })
 
   describe('in the finalized state', function () {
-    let finalized
+    let evt
 
     beforeEach(function () {
-      const proposed = new Event("Party at Frey's House")
-      proposed.proposeDate(tomorrow)
-      proposed.proposeDate(nextWeek)
-      proposed.proposeDate(nextMonth)
-      proposed.invite('frey')
-      proposed.invite('fenris')
+      evt = new Event("Party at Frey's House")
+      evt.proposeDate(tomorrow)
+      evt.proposeDate(nextWeek)
+      evt.proposeDate(nextMonth)
+      evt.invite('frey')
+      evt.invite('fenris')
 
-      proposed.acceptProposal('fenris', 2)
+      evt.acceptProposal('fenris', 2)
 
-      finalized = proposed.finalize(0)
+      evt.finalize(0)
     })
 
     it('adds invitees', function () {
-      finalized.invite('reostra')
+      evt.invite('reostra')
 
-      assert.deepEqual(finalized.getInvitees(), ['frey', 'fenris', 'reostra'])
+      assert.deepEqual(evt.getInvitees(), ['frey', 'fenris', 'reostra'])
     })
 
     it('removes invitees', function () {
-      finalized.uninvite('fenris')
+      evt.uninvite('fenris')
 
-      assert.deepEqual(finalized.getInvitees(), ['frey'])
+      assert.deepEqual(evt.getInvitees(), ['frey'])
     })
 
     it('tracks which invitees confirm or deny', function () {
-      assert.isFalse(finalized.isAttending('frey'))
-      assert.isFalse(finalized.isAttending('fenris'))
-      assert.isFalse(finalized.isAttending('reostra'))
+      assert.isFalse(evt.finalProposal().isAttending('frey'))
+      assert.isFalse(evt.finalProposal().isAttending('fenris'))
+      assert.isFalse(evt.finalProposal().isAttending('reostra'))
 
-      finalized.yes('fenris')
-      finalized.yes('reostra')
+      evt.finalProposal().yes('fenris')
+      evt.finalProposal().yes('reostra')
 
-      assert.isFalse(finalized.isAttending('frey'))
-      assert.isTrue(finalized.isAttending('fenris'))
-      assert.isTrue(finalized.isAttending('reostra'))
+      assert.isFalse(evt.finalProposal().isAttending('frey'))
+      assert.isTrue(evt.finalProposal().isAttending('fenris'))
+      assert.isTrue(evt.finalProposal().isAttending('reostra'))
     })
 
     describe('changing the finalized date', function () {
       it('resets invitee confirmation', function () {
-        finalized.yes('fenris')
-        finalized.yes('reostra')
+        evt.finalProposal().yes('fenris')
+        evt.finalProposal().yes('reostra')
 
-        finalized.refinalize(1)
+        evt.finalize(1)
 
-        assert.isFalse(finalized.isAttending('frey'))
-        assert.isFalse(finalized.isAttending('fenris'))
-        assert.isFalse(finalized.isAttending('reostra'))
+        assert.isFalse(evt.finalProposal().isAttending('frey'))
+        assert.isFalse(evt.finalProposal().isAttending('fenris'))
+        assert.isFalse(evt.finalProposal().isAttending('reostra'))
       })
 
       it('respects original proposal responses', function () {
-        finalized.yes('reostra')
-        finalized.yes('frey')
+        evt.finalProposal().yes('reostra')
+        evt.finalProposal().yes('frey')
 
-        finalized.refinalize(2)
+        evt.finalize(2)
 
-        assert.isFalse(finalized.isAttending('frey'))
-        assert.isTrue(finalized.isAttending('fenris'))
-        assert.isFalse(finalized.isAttending('reostra'))
+        assert.isFalse(evt.finalProposal().isAttending('frey'))
+        assert.isTrue(evt.finalProposal().isAttending('fenris'))
+        assert.isFalse(evt.finalProposal().isAttending('reostra'))
       })
     })
   })
