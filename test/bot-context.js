9b0aa45fdd7305bb2cd2b0817f337e6acbc7bf9c
Ash Wilson <smashwilson@github.com>
2017-12-08 11:33:37 -0500

    Refactor commands into their own modules

--- test/bot-context.js
+++ test/bot-context.js
@@ -2,7 +2,6 @@ const Helper = require('hubot-test-helper')
 const moment = require('moment-timezone')
 
 const helper = new Helper('../lib/index.js')
-const {nowFn} = require('../lib/symbols')
 
 const now = moment.tz('2017-11-18', moment.ISO_8601, 'America/New_York')
 
@@ -13,11 +12,15 @@ const nextMonth = moment.tz('2017-12-16', moment.ISO_8601, 'America/New_York')
 class BotContext {
   constructor () {
     this.room = helper.createRoom({httpd: false})
-    this.room.robot[nowFn] = userTz => now.clone().tz(userTz)
+    this.room.robot['hubot-events'].now = userTz => now.clone().tz(userTz)
   }
 
   createUser (uid, username) {
     this.room.robot.brain.userForId(uid, {name: username})
+  }
+
+  withStore (cb) {
+    return this.room.robot['hubot-events'].withStore(cb)
   }
 
   say (uid, message) {
