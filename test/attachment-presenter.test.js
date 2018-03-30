15844a97adb946d7020088bd2a8b45a57a623e96
Ash Wilson <smashwilson@gmail.com>
2018-03-29 22:18:11 -0400

    Use Invitee rendering within AttachmentPresenter

--- test/attachment-presenter.test.js
+++ test/attachment-presenter.test.js
@@ -4,10 +4,35 @@ const assert = require('chai').assert
 const {ts} = require('./bot-context')
 
 const {Event} = require('../lib/event')
+const {Invitee} = require('../lib/invitee')
 const {AttachmentPresenter} = require('../lib/attachment-presenter.js')
 
 describe('AttachmentPresenter', function () {
   let event
+
+  const u = {
+    zero: Invitee.withUID('U0'),
+    one: Invitee.withUID('U1'),
+    two: Invitee.withUID('U2')
+  }
+
+  const userSource = {
+    nameForID: uid => {
+      return {
+        U0: 'user0',
+        U1: 'user1',
+        U2: 'user2'
+      }[uid]
+    },
+
+    emailForID: uid => {
+      return {
+        U0: 'user0@foo.com',
+        U1: 'user1@bar.com',
+        U2: 'user2@baz.com'
+      }[uid]
+    }
+  }
 
   beforeEach(function () {
     event = new Event('ABC123', 'Burrito Party')
@@ -15,7 +40,7 @@ describe('AttachmentPresenter', function () {
 
   describe('with an empty event', function () {
     it('shows the title and an empty proposed dates field', function () {
-      const p = new AttachmentPresenter({})
+      const p = new AttachmentPresenter({userSource})
       const a = p.present(event)
 
       assert.equal(a.fallback, 'ABC123: Burrito Party')
@@ -32,7 +57,7 @@ describe('AttachmentPresenter', function () {
     })
 
     it('lists proposed dates', function () {
-      const p = new AttachmentPresenter({now: ts.now.getStart()})
+      const p = new AttachmentPresenter({now: ts.now.getStart(), userSource})
       const a = p.present(event)
 
       assert.deepEqual(a.fields, [{
@@ -44,15 +69,15 @@ describe('AttachmentPresenter', function () {
     })
 
     it('lists counts of attendees who have voted for each', function () {
-      event.acceptProposal('<@U123>', 0)
-      event.acceptProposal('<@U123>', 1)
+      event.acceptProposal(u.zero, 0)
+      event.acceptProposal(u.zero, 1)
 
-      event.acceptProposal('<@U456>', 0)
-      event.acceptProposal('<@U456>', 1)
+      event.acceptProposal(u.one, 0)
+      event.acceptProposal(u.one, 1)
 
-      event.acceptProposal('<@U789>', 1)
+      event.acceptProposal(u.two, 1)
 
-      const p = new AttachmentPresenter({now: ts.now.getStart()})
+      const p = new AttachmentPresenter({now: ts.now.getStart(), userSource})
       const a = p.present(event)
 
       assert.deepEqual(a.fields[0], {
@@ -64,45 +89,61 @@ describe('AttachmentPresenter', function () {
     })
 
     it('lists invitees with response status', function () {
-      event.invite('<@U000>')
-      event.invite('<@U111>')
-      event.acceptProposal('<@U111>', 0)
-      event.acceptProposal('<@U222>', 1)
+      event.invite(u.zero)
+      event.invite(u.one)
+      event.acceptProposal(u.one, 0)
+      event.acceptProposal(u.two, 1)
 
-      const p = new AttachmentPresenter({now: ts.now.getStart()})
+      const p = new AttachmentPresenter({now: ts.now.getStart(), userSource})
       const a = p.present(event)
 
       assert.deepEqual(a.fields[1], {
         title: 'Who',
         value:
           '_Responses_\n' +
-          ':white_square: <@U000> | :white_square_button: <@U111> | ' +
-          ':white_square_button: <@U222>'
+          ':white_square: user0 | :white_square_button: user1 | ' +
+          ':white_square_button: user2'
       })
     })
 
-    it('@-mentions users when requested')
+    it('@-mentions users when requested', function () {
+      event.invite(u.zero)
+      event.invite(u.one)
+      event.acceptProposal(u.one, 0)
+      event.acceptProposal(u.two, 1)
+
+      const p = new AttachmentPresenter({now: ts.now.getStart(), userSource, ping: true})
+      const a = p.present(event)
+
+      assert.deepEqual(a.fields[1], {
+        title: 'Who',
+        value:
+          '_Responses_\n' +
+          ':white_square: <@U0> | :white_square_button: <@U1> | ' +
+          ':white_square_button: <@U2>'
+      })
+    })
   })
 
   describe('with a finalized event', function () {
     beforeEach(function () {
-      event.invite('<@U000>')
-      event.invite('<@U111>')
-      event.invite('<@U222>')
+      event.invite(u.zero)
+      event.invite(u.one)
+      event.invite(u.two)
 
       event.proposeDate(ts.tomorrow)
       event.proposeDate(ts.nextWeek)
 
-      event.acceptProposal('<@U111>', 0)
-      event.acceptProposal('<@U111>', 1)
+      event.acceptProposal(u.one, 0)
+      event.acceptProposal(u.one, 1)
 
-      event.acceptProposal('<@U222>', 1)
+      event.acceptProposal(u.two, 1)
 
       event.finalize(0)
     })
 
     it('shows the chosen event date', function () {
-      const p = new AttachmentPresenter({now: ts.now.getStart()})
+      const p = new AttachmentPresenter({now: ts.now.getStart(), userSource})
       const a = p.present(event)
 
       assert.deepEqual(a.fields[0], {
@@ -112,17 +153,27 @@ describe('AttachmentPresenter', function () {
     })
 
     it('lists invitees with their response status', function () {
-      const p = new AttachmentPresenter({now: ts.now.getStart()})
+      const p = new AttachmentPresenter({now: ts.now.getStart(), userSource})
       const a = p.present(event)
 
       assert.deepEqual(a.fields[1], {
         title: 'Who',
         value:
           '_Attendees (1 confirmed)_\n' +
-          ':grey_question: <@U000> | :white_check_mark: <@U111> | :red_circle: <@U222>'
+          ':grey_question: user0 | :white_check_mark: user1 | :red_circle: user2'
       })
     })
 
-    it('@-mentions users when requested')
+    it('@-mentions users when requested', function () {
+      const p = new AttachmentPresenter({now: ts.now.getStart(), userSource, ping: true})
+      const a = p.present(event)
+
+      assert.deepEqual(a.fields[1], {
+        title: 'Who',
+        value:
+          '_Attendees (1 confirmed)_\n' +
+          ':grey_question: <@U0> | :white_check_mark: <@U1> | :red_circle: <@U2>'
+      })
+    })
   })
 })
