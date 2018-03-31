5cc6358c38f464766076e0efcf0b0cfeefd30720
Ash Wilson <smashwilson@gmail.com>
2018-03-30 22:56:37 -0400

    Render EventSets with a LinePresenter

--- test/line-presenter.test.js
+++ test/line-presenter.test.js
@@ -4,6 +4,7 @@ const assert = require('chai').assert
 const {ts} = require('./bot-context')
 
 const {Event} = require('../lib/event')
+const {EventSet} = require('../lib/event-set')
 const {LinePresenter} = require('../lib/line-presenter')
 
 describe('LinePresenter', function () {
@@ -44,6 +45,31 @@ describe('LinePresenter', function () {
     assert.strictEqual(
       line,
       '`ZZZ999` Wizard People <!date^1511596800^{date}|25 November 2017>'
+    )
+  })
+
+  it('renders an EventSet', function () {
+    const evt0 = new Event('A0', 'Zero')
+    evt0.proposeDate(ts.tomorrow)
+
+    const evt1 = new Event('B1', 'One')
+    evt1.proposeDate(ts.tomorrow)
+    evt1.proposeDate(ts.nextWeek)
+
+    const evt2 = new Event('C2', 'Two')
+    evt2.proposeDate(ts.tomorrow)
+    evt2.finalize(0)
+
+    const set = new EventSet(null, [evt0, evt1, evt2])
+
+    const p = new LinePresenter({})
+    const lines = p.present(set)
+
+    assert.strictEqual(
+      lines,
+      '`A0` _Zero_ <!date^1511078400^{date}|19 November 2017>\n' +
+      '`B1` _One_ <!date^1511078400^{date}|19 November 2017>, <!date^1511596800^{date}|25 November 2017>\n' +
+      '`C2` Two <!date^1511078400^{date}|19 November 2017>'
     )
   })
 })
