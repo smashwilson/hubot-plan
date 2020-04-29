ee396e9e4ac7eca6c97c88e6d44aae4138f9b19e
Ash Wilson <smashwilson@github.com>
2020-04-28 21:43:26 -0400

    Support add and remove actions

--- test/commands/email.test.js
+++ test/commands/email.test.js
@@ -36,18 +36,15 @@ describe("event email", function () {
     await bot.say("u0", "hubot: event email user0+added@gmail.com");
     assert.strictEqual(
       bot.response(),
-      "Email address added.\n:slack: :star: _*user0+slack@gmail.com*_, user0+added@gmail.com"
+      "Email address user0+added@gmail.com added.\n:slack: :star: _*user0+slack@gmail.com*_, user0+added@gmail.com"
     );
   });
 
   it("associates a new default email address", async function () {
-    await bot.say(
-      "u0",
-      "hubot: event email --default user0+added@gmail.com"
-    );
+    await bot.say("u0", "hubot: event email --default user0+added@gmail.com");
     assert.strictEqual(
       bot.response(),
-      "Email address added and set as your default.\n:slack: _user0+slack@gmail.com_, :star: *user0+added@gmail.com*"
+      "Email address user0+added@gmail.com added and set as your default.\n:slack: _user0+slack@gmail.com_, :star: *user0+added@gmail.com*"
     );
   });
 
@@ -58,14 +55,11 @@ describe("event email", function () {
       emailStore.add("u0", "user0+manual1@gmail.com", true);
     });
 
-    await bot.say(
-      "u0",
-      "hubot: event email --delete user0+manual0@gmail.com"
-    );
+    await bot.say("u0", "hubot: event email --delete user0+manual0@gmail.com");
     assert.strictEqual(
       bot.response(),
-      "Email address *user0+manual0@gmail.com* removed.\n" +
-        ":slack: _user0+slack@gmail.com_, :star: *user1+manual1@gmail.com*"
+      "Email address user0+manual0@gmail.com removed.\n" +
+        ":slack: _user0+slack@gmail.com_, :star: *user0+manual1@gmail.com*"
     );
   });
 });
